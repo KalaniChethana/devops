19cc1de6d753ba803d8a94002677b5cd776cceb4
KalaniChethana <kalanichethananethmi2001@gmail.com>
2026-02-14 23:26:08 +0530

    data base update

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,10 @@ app.get("/", (req, res) => {
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/neighborhood", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ MongoDB connected"))
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Atlas connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
